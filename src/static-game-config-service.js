63b9dca7f954d5abd68736bfc040b55a03c8ca7c
Beat <beat.keller@btxtech.com>
2019-09-20 13:14:28 +0200

    Bugfix, Cleanup and prepartion for land slops

--- src/static-game-config-service.js
+++ src/static-game-config-service.js
@@ -4,19 +4,21 @@ class StaticGameConfigService {
         this.slopeSkeletonConfigs = new Map();
         for (const slopeSkeletonConfig of this.staticGameConfigJson.slopeSkeletonConfigs) {
             const gui = datGui.addFolder('SlopeId: ' + slopeSkeletonConfig.internalName + ' (' + slopeSkeletonConfig.id + ')');
-            gui.add(slopeSkeletonConfig, 'fresnelOffset');
-            gui.add(slopeSkeletonConfig, 'fresnelDelta', 0.0);
-            gui.add(slopeSkeletonConfig, 'waterShininess');
-            gui.add(slopeSkeletonConfig, 'waterSpecularStrength');
-            gui.add(slopeSkeletonConfig, 'waterReflectionScale');
-            gui.add(slopeSkeletonConfig, 'waterMapScale');
-            gui.add(slopeSkeletonConfig, 'waterBumpMapDepth', 0, 1);
-            gui.add(slopeSkeletonConfig, 'waterDistortionStrength');
-            gui.add(slopeSkeletonConfig, 'waterTransparency', 0, 1);
-            gui.add(slopeSkeletonConfig, 'waterAnimationDuration');
-            gui.add(slopeSkeletonConfig, 'shallowWaterTextureScale');
-            gui.add(slopeSkeletonConfig, 'shallowDistortionStrength');
-            gui.add(slopeSkeletonConfig, 'shallowAnimation');
+            if (slopeSkeletonConfig.hasOwnProperty('waterLevel')) {
+                gui.add(slopeSkeletonConfig, 'waterFresnelOffset');
+                gui.add(slopeSkeletonConfig, 'waterFresnelDelta', 0.0);
+                gui.add(slopeSkeletonConfig, 'waterShininess');
+                gui.add(slopeSkeletonConfig, 'waterSpecularStrength');
+                gui.add(slopeSkeletonConfig, 'waterReflectionScale');
+                gui.add(slopeSkeletonConfig, 'waterMapScale');
+                gui.add(slopeSkeletonConfig, 'waterBumpMapDepth', 0, 1);
+                gui.add(slopeSkeletonConfig, 'waterDistortionStrength');
+                gui.add(slopeSkeletonConfig, 'waterTransparency', 0, 1);
+                gui.add(slopeSkeletonConfig, 'waterAnimationDuration');
+                gui.add(slopeSkeletonConfig, 'shallowWaterTextureScale');
+                gui.add(slopeSkeletonConfig, 'shallowWaterDistortionStrength');
+                gui.add(slopeSkeletonConfig, 'shallowWaterAnimation');
+            }
             slopeSkeletonConfig.wireframeSlope = false;
             gui.add(slopeSkeletonConfig, "wireframeSlope", 0, 1);
             this.slopeSkeletonConfigs.set(slopeSkeletonConfig.id, slopeSkeletonConfig);
